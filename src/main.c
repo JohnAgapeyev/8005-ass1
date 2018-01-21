0e670088c273f92a228d6f00ecf1695ac8679bac
John Agapeyev <jagapeyev@gmail.com>
2018-01-20 18:10:06 -0800

    Tweaked parameters and removed unused code

--- src/main.c
+++ src/main.c
@@ -14,72 +14,6 @@
 static const char *mixed_filename = ".input.txt";
 static void init_mixed(long count);
 
-/*
- * Calculates the nth root using Newton's method and the OpenSSL Bignum library
- */
-BIGNUM *nth_root(BIGNUM *input, const unsigned long n) {
-    int bit_count = BN_num_bits(input);
-
-    int first_bit_index = -1;
-    for (int i = bit_count; i > 0; --i) {
-        if (BN_is_bit_set(input, i)) {
-            first_bit_index = i;
-            break;
-        }
-    }
-    if (first_bit_index == -1) {
-        return NULL;
-    }
-
-    BIGNUM *power_of_two = BN_new();
-    BN_zero(power_of_two);
-    BN_set_bit(power_of_two, first_bit_index);
-
-    BN_CTX *ctx = BN_CTX_new();
-
-    BIGNUM *x = BN_dup(power_of_two);
-    BIGNUM *y = BN_new();
-
-    BIGNUM *bn_n = BN_new();
-    BN_set_word(bn_n, n);
-
-    BIGNUM *n_minus_one = BN_new();
-    BN_set_word(n_minus_one, n - 1);
-
-    BIGNUM *temp_1 = BN_new();
-    BIGNUM *temp_2 = BN_new();
-
-    BIGNUM *rtn = BN_new();
-
-    while (BN_cmp(x, BN_value_one()) == 1) {
-        BN_exp(temp_1, x, n_minus_one, ctx);
-        BN_div(temp_1, NULL, input, temp_1, ctx);
-
-        BN_mul(temp_2, n_minus_one, x, ctx);
-
-        BN_add(y, temp_1, temp_2);
-
-        BN_div(y, NULL, y, bn_n, ctx);
-
-        if (BN_cmp(y, x) != -1) {
-            BN_copy(rtn, x);
-            goto cleanup;
-        }
-        BN_copy(x, y);
-    }
-    BN_one(rtn);
-cleanup:
-    BN_free(power_of_two);
-    BN_CTX_free(ctx);
-    BN_free(x);
-    BN_free(y);
-    BN_free(bn_n);
-    BN_free(n_minus_one);
-    BN_free(temp_1);
-    BN_free(temp_2);
-    return rtn;
-}
-
 int main(int argc, char **argv) {
     long worker_count = 8;
     enum worker_type type = 0;
@@ -200,19 +134,16 @@ void *do_work(void *arg) {
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
     uint64_t delta_us = (end.tv_sec - start.tv_sec) * 1000000 + ((end.tv_nsec - start.tv_nsec) / 1000);
     printf("CPU: %lu\n", delta_us);
-
-#if 1
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     do_io_work();
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
     delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
-    printf("I/O %lu\n", delta_us);
+    printf("I/O: %lu\n", delta_us);
     clock_gettime(CLOCK_MONOTONIC_RAW, &start);
     do_mixed_work();
     clock_gettime(CLOCK_MONOTONIC_RAW, &end);
     delta_us = (end.tv_sec - start.tv_sec) * 1000000 + (end.tv_nsec - start.tv_nsec) / 1000;
-    printf("Mixed: %lu\n", delta_us);
-#endif
+    printf("Mix: %lu\n", delta_us);
     return NULL;
 }
 
@@ -224,8 +155,8 @@ void do_cpu_work(void) {
 
     BIGNUM *p = BN_new();
     BIGNUM *q = BN_new();
-    BN_generate_prime_ex(p, 40, 1, NULL, NULL, NULL);
-    BN_generate_prime_ex(q, 40, 1, NULL, NULL, NULL);
+    BN_generate_prime_ex(p, 44, 1, NULL, NULL, NULL);
+    BN_generate_prime_ex(q, 44, 1, NULL, NULL, NULL);
     BN_CTX *ctx = BN_CTX_new();
 
     BN_mul(n, p, q, ctx);
@@ -275,7 +206,7 @@ void do_cpu_work(void) {
 void do_io_work(void) {
     const char *filename = ".tmp.txt";
     int file = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-    long file_size = 1 << 22;
+    long file_size = 1 << 23;
     long read_count = 128;
     //Set file size to 1 MB
     ftruncate(file, file_size);
@@ -294,6 +225,9 @@ void do_io_work(void) {
 void do_mixed_work(void) {
     int file = open(mixed_filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     FILE *file_p = fdopen(file, "rwb");
+
+    const char *out_filename = ".mix_out.txt";
+    int output = open(out_filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
     BIGNUM *n = NULL;
     BIGNUM *x = BN_new();
@@ -314,7 +248,7 @@ void do_mixed_work(void) {
     memset(input, 0, 1024);
     while(fgets(input, 1023, file_p)) {
         input[strlen(input) - 1] = '\0';
-        BN_hex2bn(&n, input);
+        BN_dec2bn(&n, input);
 
         BN_one(d);
         BN_set_word(x, 2);
@@ -337,6 +271,17 @@ void do_mixed_work(void) {
             }
             BN_gcd(d, diff, n, ctx);
         }
+        char *dstr = BN_bn2dec(d);
+        BIGNUM *rem = BN_new();
+        BN_div(NULL, rem, n, d, ctx);
+        char *rstr = BN_bn2dec(rem);
+
+        write(output, dstr, strlen(dstr));
+        write(output, rstr, strlen(rstr));
+
+        free(dstr);
+        free(rstr);
+        BN_free(rem);
     }
     BN_free(x);
     BN_free(y);
@@ -349,26 +294,38 @@ void do_mixed_work(void) {
     BN_CTX_free(ctx);
 
     close(file);
+    close(output);
+
+    unlink(out_filename);
 }
 
 static void init_mixed(long count) {
-    BIGNUM *p = BN_new();
-    BIGNUM *q = BN_new();
-    BIGNUM *n = BN_new();
-    BN_CTX *ctx = BN_CTX_new();
-
     int file = open(mixed_filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 
-    for (long i = 0; i < 500 * count; ++i) {
-        BN_generate_prime_ex(p, 16, 1, NULL, NULL, NULL);
-        BN_generate_prime_ex(q, 16, 1, NULL, NULL, NULL);
+#pragma omp parallel for schedule(static) shared(file)
+    for (long i = 0; i < 3500 * count; ++i) {
+        BIGNUM *p = BN_new();
+        BIGNUM *q = BN_new();
+        BIGNUM *n = BN_new();
+        BN_CTX *ctx = BN_CTX_new();
+
+        BN_generate_prime_ex(p, 18, 1, NULL, NULL, NULL);
+        BN_generate_prime_ex(q, 18, 1, NULL, NULL, NULL);
         BN_mul(n, p, q, ctx);
 
-        char *n_hex = BN_bn2hex(n);
-        write(file, n_hex, strlen(n_hex));
-        write(file, "\n", 1);
+        char *n_hex = BN_bn2dec(n);
+#pragma omp critical
+        {
+            write(file, n_hex, strlen(n_hex));
+            write(file, "\n", 1);
+        }
 
         free(n_hex);
+
+        BN_free(p);
+        BN_free(q);
+        BN_free(n);
+        BN_CTX_free(ctx);
     }
 
     close(file);
